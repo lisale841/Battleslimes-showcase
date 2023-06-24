c987cfd6bca0303f6bbe138ec9bee30fa29342a6
Lisa Le <lisale841@gmail.com>
2023-06-24 16:13:44 -0700

    update data

--- src/data/slimeData.js
+++ src/data/slimeData.js
@@ -6,150 +6,150 @@ export const slimeData =
     "description": "Boo loves to hide and surprise friends and watch them giggle with delight.",
     "name": "Boo",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime4.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878053332819116033"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878053332819116033"
   }, {
     "collectorNumber": "6",
     "description": "Poo loves to bounce and play, leaving a trail of glitter wherever he goes.",
     "name": "Poo",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime6.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878055531842371585"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878055531842371585"
   }, {
     "collectorNumber": "8",
     "description":"Shark is a mischievous little slime who loves to swim and play in the waves.",
     "name": "Shark",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime8.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878057730865627137"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878057730865627137"
   }, {
     "collectorNumber": "19",
     "description": "Rainbow is the cutest slime around with vibrant colors and adorable little smile that will melt your heart!",
     "name": "Rainbow",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime19.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878069825493532673"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878069825493532673"
   }, {
     "collectorNumber": "2",
     "description": "Spark is a precious little slime that shines brighter than a diamond.",
     "name": "Spark",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime2.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878051133795860481"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878051133795860481"
   }, {
     "collectorNumber": "11",
     "description": "Bat is an adorable little slime who loves to hang upside down with big, round eyes.",
     "name": "Bat",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime11.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878061029400510465"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878061029400510465"
   }, {
     "collectorNumber": "23",
     "description": "Bat(S) is a cool slime with an edgy style and a mysterious vibe.",
     "name": "Bat(S)",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime23.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878074223540043777"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878074223540043777"
   }, {
     "collectorNumber": "12",
     "description": "Rex is a tough slime with a rugged exterior and a determined attitude.",
     "name": "Rex",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime12.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878062128912138241"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878062128912138241"
   }, {
     "collectorNumber": "13",
     "description":"Ron is a fiery little slime that blazes with energy and passion.",
     "name": "Ron",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime13.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878063228423766017"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878063228423766017"
   }, {
     "collectorNumber": "9",
     "description":"Sprout is a nature slime that embodies the beauty and wonder of the great outdoors.",
     "name": "Sprout",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime9.svg",
-    "openseaUrl": " https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878058830377254913"
+    "openseaUrl": " https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878058830377254913"
   }, {
     "collectorNumber": "24",
     "description":"Devil(S) is a fire slime with a red-hot body and a devilish charm.",
     "name": "Devil(S)",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime24.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878075323051671553"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878075323051671553"
   }, {
     "collectorNumber": "20",
     "description":"Blu(S) is a slime with a cool and calming presence.",
     "name": "Blu(S)",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime20.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878070925005160449"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878070925005160449"
   }, {
     "collectorNumber": "17",
     "description":"Shine, a radiant slime that sparkles and shimmers with a luminous glow.",
     "name": "Shine",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime17.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878067626470277121"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878067626470277121"
   }, {
     "collectorNumber": "5",
     "description":"Devil is a slime who loves to play pranks on unsuspecting adventurers.",
     "name": "Devil",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime5.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878054432330743809"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878054432330743809"
   }, {
     "collectorNumber": "16",
     "description":"Trub is a slime that possesses incredible strength.",
     "name": "Trub",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime16.svg",
-    "openseaUrl": " https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878066526958649345"
+    "openseaUrl": " https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878066526958649345"
   }, {
     "collectorNumber": "3",
     "description":"Apple is a slime whose adorable face and cheerful demeanor makes it impossible not to smile.",
     "name": "Apple",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime3.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878052233307488257"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878052233307488257"
   }, {
     "collectorNumber": "15",
     "description": "Wim's upbeat personality never fail to brighten the mood of anyone who crosses its path.",
     "name": "Wim",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime15.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878065427447021569"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878065427447021569"
   }, {
     "collectorNumber": "1",
     "description":"Blu is a slime with a bouncy and bubbly nature that is infectious.",
     "name": "Blu",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime1.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878050034284232705"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878050034284232705"
   }, {
     "collectorNumber": "18",
     "description": "Grog's rough and rocky exterior may look intimidating, but his affectionate personality will melt hearts.",
     "name": "Grog",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime18.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878068725981904897"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878068725981904897"
   }, {
     "collectorNumber": "25",
     "description": "Fae(S) with a shimmering body bounces around spreading magic and wonder wherever it goes.",
     "name": "Fae(S)",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime25.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878076422563299329"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878076422563299329"
   }, {
     "collectorNumber": "22",
     "description":"Apple(S) has a friendly personality that makes it a sweet and loyal companion.",
     "name": "Apple(S)",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime22.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878073124028416001"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878073124028416001"
   }, {
     "collectorNumber": "10",
     "description": "Wurm's playful antics never fail to bring joy to all those around it.",
     "name": "Wurm",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime10.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878059929888882689"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878059929888882689"
   }, {
     "collectorNumber": "14",
     "description":"Darky's mysterious and shadowy aura, can manipulate darkness to its advantage.",
     "name": "Darky",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime14.svg",
-    "openseaUrl": " https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878064327935393793"
+    "openseaUrl": " https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878064327935393793"
   }, {
     "collectorNumber": "21",
     "description":"Spark's electrifying personality makes it a force to be reckoned with.",
     "name": "Spark(S)",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime21.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878072024516788225"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878072024516788225"
   }, {
     "collectorNumber": "7",
     "description":"Fae is a slime with an outgoing personality who enjoys adventures.",
     "name": "Fae",
     "picture": "https://storage.googleapis.com/battleslimes_bucket/assets/images/slimecollection/BattleSlime7.svg",
-    "openseaUrl": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878056631353999361"
+    "openseaUrl": "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/38702784988183345035410978490421897305681563005849230365966878056631353999361"
   }]
 }
